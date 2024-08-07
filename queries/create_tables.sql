db0affd8e839dcec710e92f41c972cbef60c07dd
GulovM <extream2989@gmail.com>
2024-08-07 11:13:06 +0500

    change files

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,65 +1,60 @@
--- Таблица для хранения информации о странах
 create table if not exists olympics_country (
-    noc varchar(5) primary key, -- Национальный олимпийский комитет (NOC) код
-    country varchar(100) not null -- Название страны
+    noc varchar(5) primary key,
+    country varchar(100) not null
 );
 
--- Таблица для хранения биографической информации об олимпийских спортсменах
 create table if not exists olympic_athlete_bio (
-    athlete_id integer primary key, -- Уникальный идентификатор спортсмена
-    name varchar(80) not null, -- Имя спортсмена
-    sex varchar(7), -- Пол спортсмена
-    born varchar(30), -- Дата рождения спортсмена
-    height integer, -- Рост спортсмена
-    weight varchar(10), -- Вес спортсмена
-    country varchar(50) not null, -- Страна спортсмена
-    country_noc varchar(10) not null, -- NOC код страны спортсмена
-    foreign key (country_noc) references olympics_country(noc) -- Внешний ключ на таблицу olympics_country
+    athlete_id integer primary key,
+    name varchar(80) not null,
+    sex varchar(7),
+    born varchar(30),
+    height integer,
+    weight varchar(10),
+    country varchar(50) not null,
+    country_noc varchar(10) not null,
+    foreign key (country_noc) references olympics_country(noc)
 );
 
--- Таблица для хранения информации об олимпийских играх
 create table if not exists olympics_games (
-    edition varchar(100) not null, -- Издание Олимпийских игр (например, Летние или Зимние)
-    edition_id integer primary key, -- Уникальный идентификатор издания Олимпийских игр
-    year integer not null, -- Год проведения Олимпийских игр
-    city varchar(80) not null, -- Город проведения Олимпийских игр
-    country_noc varchar(5) not null, -- NOC код страны проведения Олимпийских игр
-    start_date varchar(30), -- Дата начала Олимпийских игр
-    end_date varchar(30), -- Дата окончания Олимпийских игр
-    competition_date varchar(60), -- Даты проведения соревнований
-    isHeld varchar(50), -- Статус проведения Олимпийских игр (например, "Проведены" или "Отменены")
-    foreign key (country_noc) references olympics_country(noc) -- Внешний ключ на таблицу olympics_country
+    edition varchar(100) not null,
+    edition_id integer primary key,
+    year integer not null,
+    city varchar(80) not null,
+    country_noc varchar(5) not null,
+    start_date varchar(30),
+    end_date varchar(30),
+    competition_date varchar(60),
+    isHeld varchar(50),
+    foreign key (country_noc) references olympics_country(noc)
 );
 
--- Таблица для хранения результатов событий олимпийских спортсменов
 create table if not exists olympic_athlete_event_results (
-    edition varchar(80) not null, -- Издание Олимпийских игр
-    edition_id integer not null, -- Уникальный идентификатор издания Олимпийских игр
-    country_noc varchar(5) not null, -- NOC код страны спортсмена
-    sport varchar(50) not null, -- Вид спорта
-    event text not null, -- Событие (дисциплина)
-    result_id integer, -- Уникальный идентификатор результата
-    athlete varchar(100) not null, -- Имя спортсмена
-    athlete_id integer not null, -- Уникальный идентификатор спортсмена
-    pos varchar(50), -- Позиция (место)
-    medal varchar(20), -- Тип медали (золото, серебро, бронза)
-    isTeamSport boolean not null, -- Командный спорт или индивидуальный (true или false)
-    foreign key (country_noc) references olympics_country(noc), -- Внешний ключ на таблицу olympics_country
-    foreign key (athlete_id) references olympic_athlete_bio(athlete_id), -- Внешний ключ на таблицу olympic_athlete_bio
-    foreign key (edition_id) references olympics_games(edition_id) -- Внешний ключ на таблицу olympics_games
+    edition varchar(80) not null,
+    edition_id integer not null,
+    country_noc varchar(5) not null,
+    sport varchar(50) not null,
+    event text not null,
+    result_id integer,
+    athlete varchar(100) not null,
+    athlete_id integer not null,
+    pos varchar(50),
+    medal varchar(20),
+    isTeamSport boolean not null,
+    foreign key (country_noc) references olympics_country(noc),
+    foreign key (athlete_id) references olympic_athlete_bio(athlete_id),
+    foreign key (edition_id) references olympics_games(edition_id)
 );
 
--- Таблица для хранения медального зачета по изданиям Олимпийских игр
 create table if not exists olympic_games_medal_tally (
-    edition varchar(100) not null, -- Издание Олимпийских игр
-    edition_id integer not null, -- Уникальный идентификатор издания Олимпийских игр
-    year integer not null, -- Год проведения Олимпийских игр
-    country varchar(100) not null, -- Страна
-    country_noc varchar(5) not null, -- NOC код страны
-    gold integer not null, -- Количество золотых медалей
-    silver integer not null, -- Количество серебряных медалей
-    bronze integer not null, -- Количество бронзовых медалей
-    total integer not null, -- Общее количество медалей
-    foreign key (country_noc) references olympics_country(noc), -- Внешний ключ на таблицу olympics_country
-    foreign key (edition_id) references olympics_games(edition_id) -- Внешний ключ на таблицу olympics_games
+    edition varchar(100) not null,
+    edition_id integer not null,
+    year integer not null,
+    country varchar(100) not null,
+    country_noc varchar(5) not null,
+    gold integer not null,
+    silver integer not null,
+    bronze integer not null,
+    total integer not null,
+    foreign key (country_noc) references olympics_country(noc),
+    foreign key (edition_id) references olympics_games(edition_id)
 );
--- sarter.py
+++
@@ -1,4 +0,0 @@
-from waitress import serve
-import dashboard
-if __name__ == '__main__':
-    serve(dashboard.app.server, host='0.0.0.0', port=8050)
